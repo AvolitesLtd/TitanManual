1640e2c8b6743279f0a80e5ccc871aa1a489d78d
Fraser Stockley <captainnana@gmail.com>
2019-10-22 23:21:52 +0100

    update site name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,14 +11,14 @@
 const siteConfig = {
   title: 'Unofficial Avolites Titan Manual', // Title for your website.
   tagline: 'An unofficial manual for the Avolites Titan Software',
-  url: 'https://avodocs.fraserstockley.com', // Your website URL
+  url: 'https://avolites.fraserstockley.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'avo-docs',
+  projectName: 'Avolites Titan Documentation',
   organizationName: 'Fraser Stockley',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
