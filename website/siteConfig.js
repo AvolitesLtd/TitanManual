e4eaec8796b68ee32420971388abb5a4b4932bbc
Gareth Nunns <garethnunns@gmail.com>
2020-03-19 14:29:12 +0000

    Share image

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -80,8 +80,8 @@ const siteConfig = {
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: 'img/avolites_share.jpg',
+  twitterImage: 'img/avolites_share.jpg',
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
