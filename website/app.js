c350a1e71ecdd98bc97af5f7dbf184359b81d164
Gareth Nunns <garethnunns@gmail.com>
2020-04-08 02:04:14 +0100

    App navigation buttons

--- website/app.js
+++ website/app.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow, session, protocol } = require('electron')
+const {platform} = require('os');
+const { app, BrowserWindow, session, BrowserView, ipcMain } = require('electron')
 const { readFile } = require('fs')
 const path = require('path')
 const url = require('url');
@@ -12,7 +13,7 @@ process.on('exit', () => {
   localJs.kill();
 })
 
-let win;
+let win, browserViewContent;
 
 function createWindow () {
 
@@ -20,7 +21,10 @@ function createWindow () {
   win = new BrowserWindow({
     width: 1200,
     height: 800,
+    minWidth: 550,
+    minHeight: 400,
     show: false,
+    backgroundColor: '#ffffff',
     webPreferences: {
       nodeIntegration: true,
       webSecurity: true
@@ -30,30 +34,86 @@ function createWindow () {
 
   win.setMenu(null)
 
-  win.webContents.on('did-finish-load', function() {
+  // content view
+  browserViewContent = new BrowserView({
+    backgroundColor: '#91001b',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.addBrowserView(browserViewContent)
+  let curSize = win.getSize()
+  browserViewContent.setBounds({ x: 0, y: 25, width: curSize[0], height: curSize[1]-25 })
+  browserViewContent.setAutoResize({width: true, height: true})
+  
+  browserViewContent.webContents.on('did-navigate', canNavigate)
+
+  browserViewContent.webContents.on('did-finish-load', () => {
     // add custom css
-    readFile('./app/app.css', "utf-8", function(error, data) {
+    readFile('./app/app.css', "utf-8", (error, data) => {
       if(!error){
-        win.webContents.insertCSS(data)
+        browserViewContent.webContents.insertCSS(data)
       }
     })
 
-    win.webContents.executeJavaScript(`
+    // add custom js
+    browserViewContent.webContents.executeJavaScript(`
 const bodyOnline = () => {
-  document.body.classList.add(navigator.onLine ? 'online' : 'offline')
-  document.body.classList.remove(navigator.onLine ? 'offline' : 'online')
+  $('body').addClass(navigator.onLine ? 'online' : 'offline');
+  $('body').removeClass(navigator.onLine ? 'offline' : 'online')
 }
 
 window.addEventListener('online',  bodyOnline)
 window.addEventListener('offline',  bodyOnline)
+bodyOnline()
+    `)
 
-bodyOnline();
-
-const header = document.querySelectorAll('.fixedHeaderContainer')[0];
-//header.style.backgroundColor = "blue"
-    `);
+    canNavigate()
   })
 
+  // and load the index.html of the app.
+  browserViewContent.webContents.loadURL('http://localhost:8000/index.html')
+  win.loadURL('http://localhost:8000/nav.html')
+
+  win.show();
+
+  win.on('ready-to-show',function(){
+    browserViewContent.on('ready-to-show',function(){
+      win.show();
+    })
+  })
+
+  win.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(urlFilter).then(createWindow)
+
+app.allowRendererProcessReuse = true;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  server.kill();
+  app.quit()
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// redirects
+function urlFilter() {
   const filterUrls = {
     search: {
       url: '/js/search.js',
@@ -88,7 +148,7 @@ const header = document.querySelectorAll('.fixedHeaderContainer')[0];
       if(details.url.endsWith(filterUrls[filterUrl].url)) {
         callback({ 
           cancel: false,
-          redirectURL: 'http://localhost:8001/' + filterUrls[filterUrl].file
+          redirectURL: 'http://localhost:8000/' + filterUrls[filterUrl].file
         })
 
         return
@@ -97,45 +157,38 @@ const header = document.querySelectorAll('.fixedHeaderContainer')[0];
 
     callback({ cancel: true })
   })
-
-  // and load the index.html of the app.
-  win.loadURL('http://localhost:8000/index.html')
-
-  win.on('ready-to-show',function(){
-    win.show();
-  })
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  win.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-
-app.allowRendererProcessReuse = true;
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  server.kill();
-  app.quit()
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+// window controls
+ipcMain.on("cntrl-back",function (event, arg) {
+  if(browserViewContent.webContents.canGoBack()) {
+    browserViewContent.webContents.goBack()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+ipcMain.on("cntrl-forward",function (event, arg) {
+  if(browserViewContent.webContents.canGoForward()) {
+    browserViewContent.webContents.goForward()
+  }
+})
+
+ipcMain.on("cntrl-min",function (event, arg) {
+  win.minimize()
+})
+
+ipcMain.on("cntrl-max",function (event, arg) {
+  /*if(platform() == 'darwin')
+    win.setSimpleFullScreen(!win.isSimpleFullScreen())
+  else*/
+    win.isMaximized() ? win.unmaximize() : win.maximize()
+  
+})
+
+ipcMain.on("cntrl-close",function (event, arg) {
+  win.close()
+})
+
+function canNavigate() {
+  win.webContents.send('cntrl-can-back', browserViewContent.webContents.canGoBack())
+  win.webContents.send('cntrl-can-forward', browserViewContent.webContents.canGoForward())
+}
--- website/app/server.js
+++ website/app/server.js
@@ -1,21 +1,19 @@
 const nodeStatic = require('node-static');
 const path = require('path');
-const fileServer = new nodeStatic.Server(path.resolve(__dirname,'../build/AvoDocs'))
-const sourceServer = new nodeStatic.Server(path.resolve(__dirname,'sources'))
+const fileServer = new nodeStatic.Server(path.resolve(__dirname,'../build/AvoDocs'),{ cache: false })
+const sourceServer = new nodeStatic.Server(path.resolve(__dirname,'sources'),{ cache: false })
 const { createServer } = require('http')
 
 createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response, function (e, res) {
-            if (e && (e.status === 404)) { // If the file wasn't found
-                fileServer.serveFile('/404/index.html', 404, {}, request, response);
+            if (e && (e.status === 404)) {
+                sourceServer.serve(request, response, function (e, res) {
+                    if (e && (e.status === 404)) { // If the file wasn't found
+                        fileServer.serveFile('/404/index.html', 404, {}, request, response);
+                    }
+                })
             }
         });
     }).resume();
 }).listen(8000);
-
-createServer(function (request, response) {
-    request.addListener('end', function () {
-        sourceServer.serve(request, response);
-    }).resume();
-}).listen(8001);
