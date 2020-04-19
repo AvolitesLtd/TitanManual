7d8991e66e110eff6d54bc63040a6cb6ce15edc7
Gareth Nunns <garethnunns@gmail.com>
2020-04-19 02:11:44 +0100

    App server class

--- website/app/server.js
+++ website/app/server.js
@@ -3,28 +3,64 @@ const path = require('path');
 const fileServer = new nodeStatic.Server(path.resolve(__dirname,'../build/AvoDocs'),{ cache: false })
 const sourceServer = new nodeStatic.Server(path.resolve(__dirname,'sources'),{ cache: false })
 const { createServer } = require('http')
+const EventEmitter = require('events');
 
-details = {
-  port: 8000,
-  host: 'localhost',
-  protocol: 'http'
+class appServer extends EventEmitter {
+  constructor(startingPort = 8000) {
+    super();
+
+    this.host = 'localhost'
+    this.protocol = 'http'
+
+    this.getAvailablePort(startingPort).then(port => {
+      this.port = port
+      this.run()
+    })
+  }
+
+  get url() {
+    if(!this.port)
+      throw "Port not defined"
+
+    return `${this.protocol}://${this.host}:${this.port}`
+  }
+
+  getAvailablePort(startingAt) {
+    function getNextAvailablePort (currentPort, cb) {
+        const server = createServer()
+        server.listen(currentPort, _ => {
+            server.once('close', _ => {
+                cb(currentPort)
+            })
+            server.close()
+        })
+        server.on('error', _ => {
+            getNextAvailablePort(++currentPort, cb)
+        })
+    }
+  
+    return new Promise(resolve => {
+        getNextAvailablePort(startingAt, resolve)
+    })
+  }
+
+  run(port = this.port) {
+    createServer((request, response) => {
+      request.addListener('end', () => {
+        fileServer.serve(request, response, (fe, fres) => {
+          if (fe && (fe.status === 404)) {
+            sourceServer.serve(request, response, (se, sres) => {
+              if (se && (se.status === 404)) { // If the file wasn't found
+                fileServer.serveFile('/404.html', 404, {}, request, response)
+              }
+            })
+          }
+        });
+      }).resume();
+    }).listen(port, () => {
+      this.emit('ready')
+    })
+  }
 }
-details.url = `${details.protocol}://${details.host}:${details.port}`
 
-exports.details = details
-
-exports.run = () => {
-  createServer((request, response) => {
-    request.addListener('end', () => {
-      fileServer.serve(request, response, (fe, fres) => {
-        if (fe && (fe.status === 404)) {
-          sourceServer.serve(request, response, (se, sres) => {
-            if (se && (se.status === 404)) { // If the file wasn't found
-              fileServer.serveFile('/404.html', 404, {}, request, response)
-            }
-          })
-        }
-      });
-    }).resume();
-  }).listen(details.port)
-}
+module.exports = new appServer()
