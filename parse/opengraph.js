3cff3b73d1d3a18b440f26817cddbedadc98f848
Gareth Nunns <garethnunns@gmail.com>
2020-08-31 19:56:15 +0100

    OpenGraph use parse lib

--- parse/opengraph.js
+++ parse/opengraph.js
@@ -1,22 +1,9 @@
-const { promisify } = require('util')
-const path = require('path')
 const fs = require('fs')
-const readdir = promisify(fs.readdir)
-const stat = promisify(fs.stat)
 const avoParse = require('./avoParse')
-
-async function getFiles(dir) {
-  const subdirs = await readdir(dir)
-  const files = await Promise.all(subdirs.map(async (subdir) => {
-    const res = path.resolve(dir, subdir)
-    return (await stat(res)).isDirectory() ? getFiles(res) : res
-  }));
-  return files.reduce((a, f) => a.concat(f), [])
-}
 
 console.log('Fixing OG tags');
 
-getFiles(avoParse.paths.buildDir).then((filenames) => {
+avoParse.getFiles(avoParse.paths.buildDir).then((filenames) => {
   filenames.forEach(function(filename) {
     if(filename.endsWith('.html')) {
       fs.readFile(filename,'utf-8',(err, contents) => {
