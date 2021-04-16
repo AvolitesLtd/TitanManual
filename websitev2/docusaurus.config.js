ba40686c33e8c3a7377bdbe168d58b2584dc6c06
Terry Clark <i.am.terryclark@gmail.com>
2021-04-16 16:37:58 +0100

    revert back to orignal config - WIP discussion required

--- websitev2/docusaurus.config.js
+++ websitev2/docusaurus.config.js
@@ -1,46 +1,47 @@
 module.exports={
-  title: "Avolites Manuals",
-  tagline: "Official Manual for the Avolites software",
-  url: "https://manual.avolites.com",
-  baseUrl: "/",
-  organizationName: "Avolites",
-  projectName: "AvoDocs",
-  scripts: [
+  "title": "Avolites Titan Manual",
+  "tagline": "Official Manual for the Avolites Titan software",
+  "url": "https://manual.avolites.com",
+  "baseUrl": "/",
+  "organizationName": "Avolites",
+  "projectName": "AvoDocs",
+  "scripts": [
     "https://buttons.github.io/buttons.js",
     "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js",
     "/js/fuse.js",
     "/js/search.js",
     "/js/video.js"
   ],
-  favicon: "img/favicon.ico",
-  customFields: {
-    gaGtag: true,
-    copyrightUrl: "https://avolites.com",
-    markdownPlugins: [
+  "favicon": "img/favicon.ico",
+  "customFields": {
+    "gaGtag": true,
+    "copyrightUrl": "https://avolites.com",
+    "markdownPlugins": [
       null
     ],
-    repoUrl: "https://github.com/AvolitesLtd/TitanManual"
+    "repoUrl": "https://github.com/AvolitesLtd/TitanManual"
   },
-  onBrokenLinks: "log",
-  onBrokenMarkdownLinks: "log",
-  presets: [
+  "onBrokenLinks": "log",
+  "onBrokenMarkdownLinks": "log",
+  "presets": [
     [
       "@docusaurus/preset-classic",
       {
-        docs: {
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-          path: "../docs",
-          sidebarPath: require.resolve('./sidebars.json')
+        "docs": {
+          "homePageId": "introduction",
+          "showLastUpdateAuthor": true,
+          "showLastUpdateTime": true,
+          "path": "../docs",
+          "sidebarPath": require.resolve('./sidebars.json'),
         },
-        blog: {},
-        theme: {
-          customCss: ["../static/avolites-icons/icons.css", "../src/css/customTheme.scss"]
+        "blog": {},
+        "theme": {
+          "customCss": ["../static/avolites-icons/icons.css", "../src/css/customTheme.scss"]
         }
       }
     ]
   ],
-  plugins: [
+  "plugins": [
     'docusaurus-plugin-sass',
     [
       require.resolve("./docusaurus-search-local"),
@@ -59,7 +60,7 @@ module.exports={
       },
     ]
   ],
-  i18n: {
+  "i18n": {
     defaultLocale: 'en',
     locales: ['en', 'de'],
     localeConfigs: {
@@ -71,83 +72,74 @@ module.exports={
       },
     },
   },
-  themeConfig: {
-    navbar: {
-      title: "Avolites Manuals",
-      logo: {
-        src: "img/Avolites_Logo_White.svg"
+  "themeConfig": {
+    "navbar": {
+      "title": "Avolites Titan Manual",
+      "logo": {
+        "src": "img/Avolites_Logo_White.svg"
       },
-      items: [
+      "items": [
         {
-          to: "docs/introduction",
-          label: "Titan",
-          position: "left"
+          "to": "docs/",
+          "label": "Docs",
+          "position": "left"
         },
         {
-          label: 'Prism',
-          position: 'left',
-          items: [
-            {
-              to: 'docs/prism/player/about',
-              label: 'Player',
-            },
-            {
-              to: 'docs/prism/one/about',
-              label: 'One',
-            }
-          ]
+          "to": "docs/",
+          "label": "Prism",
+          "position": "left"
         },
         {
-          to: "/help",
-          label: "Help",
-          position: "left"
+          "to": "/help",
+          "label": "Help",
+          "position": "left"
         },
         {
-          href: "https://avolites.com",
-          label: "Main Website",
-          position: "left"
+          "href": "https://avolites.com",
+          "label": "Main Website",
+          "position": "left"
         },
         {
           type: 'localeDropdown',
           position: 'right',
         },
         {
-          label: "Version",
-          to: "docs/titan",
-          position: "right",
-          items: [
+          "label": "Version",
+          "to": "docs",
+          "position": "right",
+          "items": [
             {
-              label: "14.0",
-              to: "docs/titan/",
-              activeBaseRegex: "docs/titan/(?!12.0|13.0|14.0|next)"
+              "label": "14.0",
+              "to": "docs/",
+              "activeBaseRegex": "docs/(?!12.0|13.0|14.0|next)"
             },
             {
-              label: "13.0",
-              to: "docs/titan/13.0/"
+              "label": "13.0",
+              "to": "docs/13.0/"
             },
             {
-              label: "12.0",
-              to: "docs/titan/12.0/"
+              "label": "12.0",
+              "to": "docs/12.0/"
             },
             {
-              label: "v15.0 Beta",
-              to: "docs/titan/next/",
-              activeBaseRegex: "docs/titan/next/(?!support|team|resources)"
+              "label": "v15.0 Beta",
+              "to": "docs/next/",
+              "activeBaseRegex": "docs/next/(?!support|team|resources)"
             }
           ]
         }
       ]
     },
-    image: "img/avolites_share.jpg",
-    footer: {
-      links: [
+    "image": "img/avolites_share.jpg",
+    "footer": {
+      "links": [
         {
           // Label of the section of these links
           title: 'Avolites',
           items: [
             {
               label: 'Getting Started',
-              to: 'docs/titan/',
+              to: 'docs/',
             },
             {
               label: 'Support',
@@ -189,12 +181,12 @@ module.exports={
           ]
         }
       ],
-      copyright: "Copyright Avolites 2021 ©",
-      logo: {
+      "copyright": "Copyright Avolites 2021 ©",
+      "logo": {
         "src": "img/Avolites_Logo_Red.svg"
       }
     },
-    gtag: {
+    "gtag": {
       "trackingID": "GTM-TZMDJV5"
     }
   }
