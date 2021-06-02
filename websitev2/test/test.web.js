190871d022b1716a69bcd694bc69dd00c2f33da3
Fraser Stockley <captainnana@gmail.com>
2021-06-02 12:27:16 +0100

    Adds test to check for broken links in files

---
+++ websitev2/test/test.web.js
@@ -0,0 +1,54 @@
+const { fail } = require("assert");
+var assert = require("assert"),
+  webdriver = require("selenium-webdriver");
+
+
+var buildDriver = function(caps) {
+  return new webdriver.Builder()
+    .forBrowser("chrome")
+    .build();
+};
+
+describe("Test all pages", function() {
+  var driver;
+  this.timeout(0);
+
+  beforeEach(function(done) {
+    driver = buildDriver();
+    done();
+  });
+
+  it ("Check for page", async () => {
+    await driver.get('http://localhost:3000/docs');
+    let elem = null;
+
+    do {
+      let title = await driver.getTitle();
+      console.log(title);
+      let matches = await driver.findElements(webdriver.By.css(".markdown"));
+      if (matches.length > 0) {
+        //todo figure out how to match broken files
+        const text = await matches[0].getText();
+        if (text.match(/\[.*?\]\(.*?\)/gm))
+          fail("Page contained a link that wasn't properly formatted");
+      }
+      
+      elem = await driver.findElements(webdriver.By.css(".pagination-nav__item--next .pagination-nav__link"));
+      if (elem.length > 0) {
+        await elem[0].click();
+      }
+      
+    } while (elem.length > 0);
+  });
+
+  afterEach(function(done) {
+    if (this.currentTest.isPassed) {
+      driver.executeScript("lambda-status=passed");
+    } else {
+      driver.executeScript("lambda-status=failed");
+    }
+    driver.quit().then(function() {
+      done();
+    });
+  });
+});
