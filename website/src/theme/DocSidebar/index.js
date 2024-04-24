76e8a604ffb8fe0d5eba9dbcc80ba19edbbfc059
AndreaAvolites <andrea.campisi@avolites.com>
2024-04-24 13:50:12 +0100

    Added version string and updated style

--- website/src/theme/DocSidebar/index.js
+++ website/src/theme/DocSidebar/index.js
@@ -3,17 +3,21 @@ import DocSidebar from '@theme-original/DocSidebar';
 import './style.css'
 
 export default function DocSidebarWrapper(props) {
-  const isPrism = props.path.includes('prism')
-  
+  const isPrism = props.path.includes('prism/prism')
+  const isZero = props.path.includes('prism/zero')
+  const isPlayer = props.path.includes('prism/player')
+
   //Use the props to grab data from the sidebar config
   console.log(props);
 
+  const version = (isZero || isPlayer) ? "v1.2" : isPrism ? "v1.3" : ""; 
+
   return (
-      isPrism ?
+      (isPrism || isZero || isPlayer) ?
         <>
           <div className='sideBarHeader'>
             <span className='theme-doc-version-badge badge badge--secondary'>
-              Version: insert here!
+              Version: {version}
             </span>
           </div>
           <div className='sidebarOffset'>
