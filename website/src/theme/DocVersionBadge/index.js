cc20aa41ca1aaba65781d5c7034472fb6e62bea1
Terry Clark <i.am.terryclark@gmail.com>
2024-04-25 16:30:48 +0100

    added version badge to docs
    custom component added for versioning

---
+++ website/src/theme/DocVersionBadge/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useLocation } from "react-router";
+import DocVersionBadge from '@theme-original/DocVersionBadge';
+
+export default function DocVersionBadgeWrapper(props) {
+  const pathName = useLocation().pathname;
+  const isPrism = pathName.includes('prism')
+  
+  return (
+    isPrism ?
+      <>
+        <span className='theme-doc-version-badge badge badge--secondary'>
+          Version: 1.3
+        </span>
+        <DocVersionBadge {...props} />
+      </>
+      : 
+    <DocVersionBadge {...props} />
+  );
+}
