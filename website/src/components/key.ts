99c1f749ee509b1dabdd41db60ce3e046ea7bfb9
Terry Clark <i.am.terryclark@gmail.com>
2024-06-05 09:13:43 +0100

    Fixed style to show icons on keys or just the styled div around text

--- website/src/components/key.ts
+++ website/src/components/key.ts
@@ -4,19 +4,19 @@ const transButtons = require('../../languages-buttons.json')
 const Key = (tag: string) => {
     return (props) => {
       
-      const buttons = tag === 'prism' ? transButtons.context : transButtons.prism;
+      const buttons = tag === 'prism' ? transButtons.prism : transButtons.context;
       
-      let extraClass = '';
+      let extraClass = ''; 
 
       for (const [cls, trans] of Object.entries(buttons))
         for (const [_, tran] of Object.entries(trans['lang']))
           if(typeof props.children == 'string' && props.children.toLowerCase() == tran)
             extraClass += cls;
-          else {
-            //We want to style things inside PrismKeys without icons
-            if (tag === 'prism')
-              extraClass = 'prism-custom'
-          }
+          
+      //We want to style things inside PrismKeys without icons
+      if (tag === 'prism' && extraClass === '')
+        extraClass = 'prism-custom'
+      
       return React.createElement('span', {className: `${tag} ${extraClass}`, ...props});
     };
   };
