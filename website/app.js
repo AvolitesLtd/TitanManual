81590bb2014cf89c30c2a28cc271b9f26bb37946
Terry Clark <i.am.terryclark@gmail.com>
2025-06-12 11:55:52 +0100

    reordered clear calls

--- website/app.js
+++ website/app.js
@@ -51,14 +51,13 @@ const checkSingleInstance = (fullAppName) => {
 }
 
 const createWindow = () => {
+  // Activating the window of primary instance when a second instance starts
+  if (!checkSingleInstance("Avolites Manual"))
+    return false;
 
   session?.defaultSession?.clearCache().then( e => console.log(e, "cleared cache"));
 
   session?.defaultSession?.clearStorageData().then( e => console.log(e, "cleared storage data"));
-
-  // Activating the window of primary instance when a second instance starts
-  if (!checkSingleInstance("Avolites Manual"))
-    return false;
 
   // Create the browser window.
   win = new BrowserWindow({
